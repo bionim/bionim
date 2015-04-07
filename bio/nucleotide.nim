835965c46cea3605dbe879c5c2a01485002dc96a
Adel Qalieh <aqalieh95@gmail.com>
2015-04-07 18:11:31 -0400

    Add nucleotide type

---
+++ bio/nucleotide.nim
@@ -0,0 +1,16 @@
+## Nucleotide ##
+
+type Nucleotide = char
+
+## Conversions ##
+proc dna2rna(s: seq[Nucleotide]): seq[Nucleotide] =
+  result = s
+  for i in low(s)..high(s):
+    if (int(s[i]) or 32) == 116:
+      result[i] = char(int(s[i]) + 1)
+
+proc rna2dna(s: seq[Nucleotide]): seq[Nucleotide] =
+  result = s
+  for i in low(s)..high(s):
+    if (int(s[i]) or 32) == 116:
+      result[i] = char(int(s[i]) - 1)
